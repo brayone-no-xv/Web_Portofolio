59b9f27669d408e172d934dbd3fbabfa4e0d7dcf
Muhammad Rahman <hrepunzel@gmail.com>
2025-04-03 21:23:33 +0700

    🔃 refactor : include navbar, hero, about, skill, and footer sections - Portofolio.vue

---
+++ main/portofolio/src/Portofolio.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import navbar from "./components/navbar/navbar.vue";
+import heroSection from "./components/hero/hero.vue";
+import aboutSection from "./components/about/about.vue";
+import skillSection from "./components/skill/skill.vue";
+import footerSection from "./components/footer/footer.vue";
+</script>
+
+<template>
+  <header>
+    <!-- navbar -->
+    <navbar />
+  </header>
+  <main>
+    <!-- hero -->
+    <heroSection />
+    <!-- about -->
+    <aboutSection />
+    <!-- skill -->
+    <skillSection />
+  </main>
+  <footer>
+    <!-- footer -->
+    <footerSection />
+  </footer>
+</template>
+
+<style scoped></style>
